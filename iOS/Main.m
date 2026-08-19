df20c4e46eeb62da75e7f39637a7e66a3535bfac
kaylabsibley09-cloud <kaylabsibley09@gmail.com>
2026-08-18 21:00:53 -0400

    Update Main.m

--- iOS/Main.m
+++ iOS/Main.m
@@ -3,7 +3,7 @@
 #import <Foundation/Foundation.h>
 #import <objc/runtime.h>
 
-#define API_URL @"http://26.186.108.11:3551"
+#define API_URL @"http://127.0.0.1:3551"
 #define EPIC_GAMES_URL @"ol.epicgames.com"
 //67
 @interface CustomURLProtocol : NSURLProtocol
